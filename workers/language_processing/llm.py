c193df5ad079305f9326f989f21f9411df5847e9
maxime <maximebatista18@gmail.com>
2024-10-31 14:10:24 +0100

    WIP

--- workers/language_processing/llm.py
+++ workers/language_processing/llm.py
@@ -1,36 +1,15 @@
 from llama_cpp import Llama, LlamaGrammar
 
-with open("./tests.txt", "r") as f:
-    text = ''.join(f.readlines())
-
-llama = Llama("./mistral-7b-instruct-v0.1.Q4_K_M.gguf", n_gpu_layers=10000, n_ctx=8192, gpu_reserve_mem=512, n_batch=256)
-
-print("---")
-print("---")
-
-# schema = r'''
-# root ::= Entitylist
-# Entity ::= "{"   ws   "\"name\":"   ws   string   ","   ws   "\"type\":"   ws   string   "}"
-# Entitylist ::= "[]" | "["   ws   Entity   (","   ws   Entity)*   "]"
-# Answer ::= "{"   ws   "\"results\":"   ws   Entitylist   "}"
-# Answerlist ::= "[]" | "["   ws   Answer   (","   ws   Answer)*   "]"
-# string ::= "\""   ([^"]*)   "\""
-# boolean ::= "true" | "false"
-# ws ::= [ \t\n]*
-# number ::= [0-9]+   "."?   [0-9]*
-# stringlist ::= "["   ws   "]" | "["   ws   string   (","   ws   string)*   ws   "]"
-# numberlist ::= "["   ws   "]" | "["   ws   string   (","   ws   number)*   ws   "]"
-#
-# '''
+# llama = Llama("./Mistral-7B-Instruct-v0.3.Q8_0.gguf", n_gpu_layers=200, n_ctx=8192, gpu_reserve_mem=512, n_batch=1024, n_threads=20, n_threads_batch=20)
+llama = Llama("./Mistral-7B-Instruct-v0.3.Q8_0.gguf", n_gpu_layers=20, n_ctx=8192, n_batch=256, n_threads=20, n_threads_batch=20)
 
 schema = r'''
-root ::= Entities
-Information ::= "{"   ws   "\"type\":"   ws   string   ","   ws   "\"value\":"   ws   string   "}"
+root ::= "summary:" string "\n" "entities:" Entitylist
+Information ::= "{"   ws   "\"type\":"   ws   ("\"profession\"" | "\"address\"" | "\"service\"" | "\"product\"" )   ","   ws   "\"value\":"   ws   string   "}"
 Informationlist ::= "[]" | "["   ws   Information   (","   ws   Information)*   "]"
-Entity ::= "{"   ws   "\"name\":"   ws   string   ","   ws   "\"type\":"   ws   string   ","   ws   "\"informations\":"   ws   Informationlist   "}"
+Entity ::= "{"   ws   "\"type\":"   ws   (("\"enterprise\", \"products and services\":" stringlist) | "\"person\"" | "\"activity\"") ","   ws   "\"name\":"   ws   string   ","   ws   "\"information\":"   ws   Informationlist   "}"
 Entitylist ::= "[]" | "["   ws   Entity   (","   ws   Entity)*   "]"
-Entities ::= "{"   ws   "\"entities\":"   ws   Entitylist   "}"
-Entitieslist ::= "[]" | "["   ws   Entities   (","   ws   Entities)*   "]"
+Reply ::= "{"   ws   "\"summary\":"   ws   string   ","   ws   "\"entities\":"   ws   Entitylist   "}"
 string ::= "\""   ([^"]*)   "\""
 boolean ::= "true" | "false"
 ws ::= [ \t\n]*
@@ -41,10 +20,20 @@ numberlist ::= "["   ws   "]" | "["   ws   string   (","   ws   number)*   ws   
 
 grammar = LlamaGrammar.from_string(grammar=schema, verbose=False)
 
-response = llama.create_completion(text, grammar=grammar, temperature=0.7, min_p=0, repeat_penalty=1, frequency_penalty=0, presence_penalty=0, max_tokens=8192, stream=True)
-# response = llama.create_completion("Generate a JSON containing 5 cars associated with their fictive owner : their features, their owners.", grammar=grammar, temperature=0, max_tokens=8192, stream=True)
+def analyze_text(text: str):
+    prompt = f"""
+        You are an email analyst assistant. You are used to understand a text in its details, and will formulate a detailed reply that will be composed of two detailed parts :
+        First part (summary), you will provide a detailed summary of the email. You will tell what is its purpose, what the sender aims for, and try to explain if the mail tries to get in touch with the receiver, sell/propose services, is a regular conversation or a notification etc. The summary is detailed but cant exceed more than 7 sentences, minimum 5 sentences (you can put multiple information per sentences).
+        Second part (entities), you will collect all the person names and enterprises mentioned ONLY (not products or services). You cant repeat yourself, be aware of synonyms. For each of enterprises and person names, you'll provide a list of ALL bound information (even the most implicit ones) such as their addresses, professions, products and services.
+        Here is the text :
+        {text}
+    """
+    response = llama.create_completion(prompt, grammar=grammar, temperature=0, repeat_penalty=1.1, frequency_penalty=1, presence_penalty=0, max_tokens=4096, stream=True)
+    # response = llama.create_completion("Generate a JSON containing 5 cars associated with their fictive owner : their features, their owners.", grammar=grammar, temperature=0, max_tokens=8192, stream=True)
 
-for item in response:
-    print(item["choices"][0]["text"], end='', flush=True)
+    for item in response:
+        token = item["choices"][0]["text"]
+        print(token, end='', flush=True)
+        yield token
 
 
--- workers/language_processing/main.py
+++ workers/language_processing/main.py
@@ -2,20 +2,12 @@
 
 from flask import Flask, request
 
-from ner import scan_text
 import json
 
-from ner import analyze_text
+from llm import analyze_text
 from ocr import extract_text_from_image
 
 app = Flask(__name__)
-
-@app.post('/ner')
-def ner():
-    text = request.data.decode('utf-8')
-    # result = scan_text(text)
-    result = analyze_text(text)
-    return json.dumps(result.__dict__)
 
 @app.post('/ocr')
 def ocr():
@@ -29,5 +21,9 @@ def ocr():
         texts[url] = extract_text_from_image(url)
 
     return json.dumps(texts)
+
+@app.post("/llm/analyze")
+def text_analysis():
+    return analyze_text(request.data.decode('utf-8')), {"Content-Type": "application/text"}
 
 app.run(debug=True, port=8080, host='0.0.0.0')
